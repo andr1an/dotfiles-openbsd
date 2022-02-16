e02a5e8f053fd987bca8a11e2354127dce757394
Sergey Andrianov <info@andrian.ninja>
2022-02-16 13:38:58 +0300

    Add FontAwesome for i3status

--- install_stuff.sh
+++ install_stuff.sh
@@ -8,10 +8,12 @@ doas sed -i.bak 's,^[^#].*/bin/xconsole,#&,' /etc/X11/xenodm/Xsetup_0
 
 curl -fSL -o "OpenSansCondensed.zip" "https://fonts.google.com/download?family=Open%20Sans%20Condensed"
 curl -fSL -o "Input-Font.zip" "https://input.djr.com/build/?fontSelection=fourStyleFamily&regular=InputSansCondensed-Regular&italic=InputMonoCondensed-Italic&bold=InputMonoCondensed-Bold&boldItalic=InputMonoCondensed-BoldItalic&a=0&g=0&i=0&l=0&zero=0&asterisk=0&braces=0&preset=default&line-height=1.2&accept=I+do&email="
+curl -fSL -o "FontAwesome.zip" "https://use.fontawesome.com/releases/v6.0.0/fontawesome-free-6.0.0-desktop.zip"
 
-mkdir -p ~/.fonts/Input ~/.fonts/OpenSansCondensed
+mkdir -p ~/.fonts/Input ~/.fonts/OpenSansCondensed ~/.fonts/FontAwesome
 
 unzip -j Input-Font.zip "Input_Fonts/Input/*.ttf" -d ~/.fonts/Input
 unzip -j OpenSansCondensed.zip "*.ttf" -d ~/.fonts/OpenSansCondensed
+unzip -j FontAwesome.zip "fontawesome-free-6.0.0-desktop/otfs/*.otf" -d ~/.fonts/FontAwesome
 
 fc-cache
